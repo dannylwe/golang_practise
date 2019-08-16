83d8095264ecd3a79e61be0a8dfc666dbc81df83
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-08-16 23:44:43 +0300

    add rectangle struct

--- structs, methods & interfaces/shapes.go
+++ structs, methods & interfaces/shapes.go
@@ -1,11 +1,17 @@
 package shapes
 
+// Reactangle struct to define a rectangle
+type Rectangle struct {
+	Width float64
+	Length float64
+}
+
 // Perimeter function gets the 2*length + 2*width the perimeter of a rectangle
-func Perimeter(length float64, width float64) float64{
-	return 2 * (length + width)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Length + rectangle.Width)
 }
 
 //Area function finds the area of a rectangle
-func Area(length float64, width float64) float64{
-	return (length * width)
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Length * rectangle.Width)
 }
