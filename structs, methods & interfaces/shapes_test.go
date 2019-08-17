32fd6a3916cc30b9cc311a14eca55c25b9f947c1
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-08-17 10:47:08 +0300

    refactor test area tests

--- structs, methods & interfaces/shapes_test.go
+++ structs, methods & interfaces/shapes_test.go
@@ -27,6 +27,21 @@ func TestArea(t *testing.T) {
 		circle := Circle{10.0}
 		want := 314.1592653589793
 		checkArea(t, circle, want)
-	})
-	
+	})	
+}
+
+func TestAreaRefactor(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
 }
