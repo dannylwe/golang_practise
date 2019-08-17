346c54abe720a60fb65ad84f3f6336e6c1beeca2
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-08-17 14:58:42 +0300

    preliminary use of pointer

--- structs, methods & interfaces/more on methods/employee.go
+++ structs, methods & interfaces/more on methods/employee.go
@@ -9,10 +9,19 @@ type Employee struct {
 func (e Employee) FullName() string {
 	return e.FirstName + " " + e.LastName
 }
+
+// function changes first name. Without pointer, it creates copy
+func (e *Employee) changeNameFirstName(newFirstName string) {
+	e.FirstName = newFirstName
+}
+
 func main() {
 	employee := Employee{
 		FirstName: "Daniel",
 		LastName:  "Roy",
 	}
+	fmt.Println(employee.FullName())
+	println("changing first name...\n")
+	employee.changeNameFirstName("Danny Boy")
 	fmt.Println(employee.FullName())
 }
