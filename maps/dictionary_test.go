fcf91276dc45a7a6e66e19b716ff758ca601287b
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-08-27 02:34:01 +0300

    handle error in search

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,13 +3,25 @@ package dict
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 	
-	// search by key
-	got := dictionary.Search("test")
-	want := "this is a test"
-	assertStrings(t, got, want)
+    t.Run("known word", func(t *testing.T) {
+        got, _ := dictionary.Search("test")
+        want := "this is just a test"
 
+        assertStrings(t, got, want)
+    })
+
+    t.Run("unknown word", func(t *testing.T) {
+        _, err := dictionary.Search("unknown")
+        want := "could not find the word you were looking for"
+
+        if err == nil {
+            t.Fatal("expected to get an error.")
+        }
+		// errors can be converted to strings using the .Error() method
+        assertStrings(t, err.Error(), want)
+    })
 }
 
 func assertStrings(t *testing.T, got, want string) {
