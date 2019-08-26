fde6e73f0c607c0dc3e152ae86f6b9c81f554f71
Daniel Lwetabe <danny@Daniels-MacBook-Pro-3.local>
2019-08-27 02:05:50 +0300

    refactor tests

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -8,7 +8,12 @@ func TestSearch(t *testing.T) {
 	// search by key
 	got := Search(dictionary, "test")
 	want := "this is a test"
+	assertStrings(t, got, want)
 
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("wanted '%s' but got '%s' given '%s'", want, got, "test")
 	}
