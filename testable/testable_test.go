13032069bbe78dc03ee28ce9b70068d26c800578
Daniel Lwetabe <acehanks@gmail.com>
2020-05-30 14:50:30 +0300

    table tests

--- testable/testable_test.go
+++ testable/testable_test.go
@@ -3,8 +3,27 @@ package testable
 import "testing"
 
 func TestStrInSlice(t *testing.T){
-	got := StrInSlice([]string{"a", "b"}, "c")
-	if got != false {
-		t.Errorf("expecting false got true")
+	
+	var tests = []struct {
+		slice []string
+		find string
+		want bool
+	}{
+		{[]string{"a", "b"}, "c", false},
+		{[]string{"a", "b"}, "b", true},
+	}
+
+	// got := StrInSlice([]string{"a", "b"}, "c")
+	// if got != false {
+	// 	t.Errorf("expecting false got true")
+	// }
+
+	for _, test := range tests {
+		t.Run("table tests for slice", func(t *testing.T){
+			got:= StrInSlice(test.slice, test.find)
+			if got != test.want {
+				t.Errorf("expecting %v got %v", test.want, test.find)
+			}
+		})
 	}
 }
